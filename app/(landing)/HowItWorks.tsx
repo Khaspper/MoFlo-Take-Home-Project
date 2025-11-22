d839b9cc2bce6219f09ed95f0cec2edb54c74180
Mark <brandonnarciso24@gmail.com>
2025-11-22 15:13:57 -0800

    feat: Finish making the How it works section

---
+++ app/(landing)/HowItWorks.tsx
@@ -0,0 +1,44 @@
+export default function HowItWorks() {
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <h1 className="text-4xl text-[#464646]">How it works</h1>
+      <div className="bg-[#F9FAFB] py-6 px-8 rounded-lg w-[800px] flex flex-col gap-8">
+        <section>
+          <h1 className="text-3xl">
+            <span className="text-[#464646]">1.</span> Enter Property Details
+          </h1>
+          <p className="text-[#817E7E] text-">
+            Add the home&apos;s basics like bedrooms, bathrooms, price and
+            square footage.
+          </p>
+        </section>
+        <section>
+          <h1 className="text-3xl">
+            <span className="text-[#464646]">2.</span> Upload Photos
+          </h1>
+          <p className="text-[#817E7E] text-">
+            Drop in your property images to complete the listing profile.
+          </p>
+        </section>
+        <section>
+          <h1 className="text-3xl">
+            <span className="text-[#464646]">3.</span> Generate Listing Content
+          </h1>
+          <p className="text-[#817E7E] text-">
+            MoListings instantly creates polished descriptions and highlights
+            based on your inputs.
+          </p>
+        </section>
+        <section>
+          <h1 className="text-3xl">
+            <span className="text-[#464646]">4.</span> Review & Publish
+          </h1>
+          <p className="text-[#817E7E] text-">
+            Preview the final listing and save it to use across any platform or
+            marketing channel.
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
-import Hero from "./(landing)/Hero";
 import Image from "next/image";
+import Hero from "./(landing)/Hero";
+import HowItWorks from "./(landing)/HowItWorks";
 
 export default function Home() {
   return (
-    <div className="flex flex-col sm:flex-row justify-center items-center flex-wrap p-4 py-10">
-      <Hero />
-      <div className="w-full sm:w-[400px] ml-auto md:ml-0">
-        <Image
-          src="/img/new-listing-image.png"
-          alt="MoFlo logo"
-          width={500}
-          height={400}
-          className="w-[500px] h-auto"
-        />
+    <>
+      <div className="flex flex-col sm:flex-row justify-center items-center flex-wrap p-4 pt-[100px] pb-[200px]">
+        <Hero />
+        <div className="w-full sm:w-[400px] ml-auto md:ml-0">
+          <Image
+            src="/img/new-listing-image.png"
+            alt="MoFlo logo"
+            width={500}
+            height={400}
+            className="w-[500px] h-auto"
+          />
+        </div>
       </div>
-    </div>
+      <HowItWorks />
+    </>
   );
 }
